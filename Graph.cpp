e75b2a3fc89f86cedd2efceab0f622cf59654183
ckugel <53205881+ckugel@users.noreply.github.com>
2021-11-10 12:20:35 -0600

    Update Graph.cpp

--- Graph.cpp
+++ Graph.cpp
@@ -10,3 +10,27 @@ void Graph<V>::playGround() {
   head = tempNode;
   
 }
+
+template<typename V>
+int Graph<V>::findNode(Node<V>* node) {
+  for (int nodeFinder = 0; nodeFinder < nodes.size(); nodeFinder++) {
+    if (nodes[nodefinder] == tempNode) {
+      return nodeFinder;
+    }
+  }
+}
+
+
+template<typename V>
+void Graph<V>::matrixShananigans(Node<V>* nextNode, vector<Node<V>*> adjacentNodes) {
+  head = nextNode;
+  nodes.push_back(node);
+  for (int looper = 0; looper < adjacentNodes.size(); looper++) {
+    int currNodeNum = findNode(nextNode);
+    int nextNodeNum = findNode(adjacentNodes[looper]);
+    matrix[currNodeNum][nextNodeNum] = 1;
+    std::swap(currNodeNum, nextNodeNum);
+    matrix[currNodeNum][nextNodeNum] = 1;
+    std::swap(currNodeNum, nextNodeNum);
+  }
+}
